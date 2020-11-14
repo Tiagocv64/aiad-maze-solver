eac2e24697a3b44665cb4235ff461997cf42fe75
Diogo <diogosousa990@gmail.com>
2020-11-14 19:44:44 +0000

    reinstated part of the removed code

--- src/main/java/Agents/AgentMessage.java
+++ src/main/java/Agents/AgentMessage.java
@@ -16,6 +16,7 @@ public class AgentMessage implements Serializable {
     public static final String INFORM_DOOR_OPEN = "INFORM_DOOR_OPEN";
     public static final String PROPOSE = "PROPOSE";
     public static final String LOOKING_FOR_BUTTON = "LOOKING_FOR_BUTTON";
+    public static final String OPEN_DOOR = "OPEN_DOOR";
 
     private final AID sender;  // Who sent the message
     private final String description;  // What is in the message
--- src/main/java/Maze/Door.java
+++ src/main/java/Maze/Door.java
@@ -7,6 +7,7 @@ public class Door implements Serializable {
     private Button button;
     private Color color;
     private int number;
+    private boolean open = false;
 
     public Door(Color color, int number) {
         this.color = color;
@@ -29,4 +30,11 @@ public class Door implements Serializable {
         return number;
     }
 
+    public void openDoor() {
+        this.open = true;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
 }
